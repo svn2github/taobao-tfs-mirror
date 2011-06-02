546c89c10f7decdfd62ac3d8b7301001a2c2bbbf
zongdai@taobao.com <zongdai@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-06-02 05:36:22 +0000

    see http://rb.corp.taobao.com/r/11251/
    git-svn-id: http://code.taobao.org/svn/tfs@393 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-1.4.0/src/message/message_factory.cpp
+++ branches/tfs-dev-1.4.0/src/message/message_factory.cpp
@@ -28,7 +28,7 @@ namespace tfs
         switch (real_pcode)
         {
           case common::GET_BLOCK_INFO_MESSAGE:
-            packet = new GetBlockInfoMessage(); 
+            packet = new GetBlockInfoMessage();
             break;
           case common::SET_BLOCK_INFO_MESSAGE:
             packet = new  SetBlockInfoMessage();
@@ -37,148 +37,148 @@ namespace tfs
             packet = new  BatchGetBlockInfoMessage();
             break;
           case common::BATCH_SET_BLOCK_INFO_MESSAGE:
-            packet = new  BatchSetBlockInfoMessage(); 
+            packet = new  BatchSetBlockInfoMessage();
             break;
           case common::CARRY_BLOCK_MESSAGE:
-            packet = new  CarryBlockMessage(); 
+            packet = new  CarryBlockMessage();
             break;
           case common::SET_DATASERVER_MESSAGE:
-            packet = new  SetDataserverMessage(); 
+            packet = new  SetDataserverMessage();
             break;
           case common::UPDATE_BLOCK_INFO_MESSAGE:
-            packet = new  UpdateBlockInfoMessage(); 
+            packet = new  UpdateBlockInfoMessage();
             break;
           case common::BLOCK_WRITE_COMPLETE_MESSAGE:
             packet = new  BlockWriteCompleteMessage();
             break;
           case common::READ_DATA_MESSAGE:
-            packet = new  ReadDataMessage(); 
+            packet = new  ReadDataMessage();
             break;
           case common::RESP_READ_DATA_MESSAGE:
-            packet = new  RespReadDataMessage(); 
+            packet = new  RespReadDataMessage();
             break;
           case common::FILE_INFO_MESSAGE:
-            packet = new  FileInfoMessage(); 
+            packet = new  FileInfoMessage();
             break;
           case common::RESP_FILE_INFO_MESSAGE:
-            packet = new  RespFileInfoMessage(); 
+            packet = new  RespFileInfoMessage();
             break;
           case common::WRITE_DATA_MESSAGE:
-            packet = new  WriteDataMessage(); 
+            packet = new  WriteDataMessage();
             break;
-          case common::CLOSE_FILE_MESSAGE: 
-            packet = new  CloseFileMessage(); 
+          case common::CLOSE_FILE_MESSAGE:
+            packet = new  CloseFileMessage();
             break;
-          case common::UNLINK_FILE_MESSAGE: 
-            packet = new  UnlinkFileMessage(); 
+          case common::UNLINK_FILE_MESSAGE:
+            packet = new  UnlinkFileMessage();
             break;
-          case common::REPLICATE_BLOCK_MESSAGE: 
-            packet = new  ReplicateBlockMessage(); 
+          case common::REPLICATE_BLOCK_MESSAGE:
+            packet = new  ReplicateBlockMessage();
             break;
-          case common::COMPACT_BLOCK_MESSAGE: 
-            packet = new  CompactBlockMessage(); 
+          case common::COMPACT_BLOCK_MESSAGE:
+            packet = new  CompactBlockMessage();
             break;
-          case common::GET_SERVER_STATUS_MESSAGE: 
-            packet = new  GetServerStatusMessage(); 
+          case common::GET_SERVER_STATUS_MESSAGE:
+            packet = new  GetServerStatusMessage();
             break;
-          case common::SUSPECT_DATASERVER_MESSAGE: 
-            packet = new  SuspectDataserverMessage(); 
+          case common::SUSPECT_DATASERVER_MESSAGE:
+            packet = new  SuspectDataserverMessage();
             break;
-          case common::RENAME_FILE_MESSAGE: 
-            packet = new  RenameFileMessage(); 
+          case common::RENAME_FILE_MESSAGE:
+            packet = new  RenameFileMessage();
             break;
-          case common::CLIENT_CMD_MESSAGE: 
-            packet = new  ClientCmdMessage(); 
+          case common::CLIENT_CMD_MESSAGE:
+            packet = new  ClientCmdMessage();
             break;
-          case common::CREATE_FILENAME_MESSAGE: 
-            packet = new  CreateFilenameMessage(); 
+          case common::CREATE_FILENAME_MESSAGE:
+            packet = new  CreateFilenameMessage();
             break;
-          case common::RESP_CREATE_FILENAME_MESSAGE: 
-            packet = new  RespCreateFilenameMessage(); 
+          case common::RESP_CREATE_FILENAME_MESSAGE:
+            packet = new  RespCreateFilenameMessage();
             break;
-          case common::ROLLBACK_MESSAGE: 
-            packet = new  RollbackMessage(); 
+          case common::ROLLBACK_MESSAGE:
+            packet = new  RollbackMessage();
             break;
-          case common::RESP_HEART_MESSAGE: 
-            packet = new  RespHeartMessage(); 
+          case common::RESP_HEART_MESSAGE:
+            packet = new  RespHeartMessage();
             break;
-          case common::RESET_BLOCK_VERSION_MESSAGE: 
-            packet = new  ResetBlockVersionMessage(); 
+          case common::RESET_BLOCK_VERSION_MESSAGE:
+            packet = new  ResetBlockVersionMessage();
             break;
-          case common::BLOCK_FILE_INFO_MESSAGE: 
-            packet = new  BlockFileInfoMessage(); 
+          case common::BLOCK_FILE_INFO_MESSAGE:
+            packet = new  BlockFileInfoMessage();
             break;
-          case common::NEW_BLOCK_MESSAGE: 
-            packet = new  NewBlockMessage(); 
+          case common::NEW_BLOCK_MESSAGE:
+            packet = new  NewBlockMessage();
             break;
-          case common::REMOVE_BLOCK_MESSAGE: 
-            packet = new  RemoveBlockMessage(); 
+          case common::REMOVE_BLOCK_MESSAGE:
+            packet = new  RemoveBlockMessage();
             break;
-          case common::LIST_BLOCK_MESSAGE: 
-            packet = new  ListBlockMessage(); 
+          case common::LIST_BLOCK_MESSAGE:
+            packet = new  ListBlockMessage();
             break;
-          case common::RESP_LIST_BLOCK_MESSAGE: 
-            packet = new  RespListBlockMessage(); 
+          case common::RESP_LIST_BLOCK_MESSAGE:
+            packet = new  RespListBlockMessage();
             break;
-          case common::BLOCK_RAW_META_MESSAGE: 
-            packet = new  BlockRawMetaMessage(); 
+          case common::BLOCK_RAW_META_MESSAGE:
+            packet = new  BlockRawMetaMessage();
             break;
-          case common::WRITE_RAW_DATA_MESSAGE: 
-            packet = new  WriteRawDataMessage(); 
+          case common::WRITE_RAW_DATA_MESSAGE:
+            packet = new  WriteRawDataMessage();
             break;
-          case common::WRITE_INFO_BATCH_MESSAGE: 
-            packet = new  WriteInfoBatchMessage(); 
+          case common::WRITE_INFO_BATCH_MESSAGE:
+            packet = new  WriteInfoBatchMessage();
             break;
-          case common::BLOCK_COMPACT_COMPLETE_MESSAGE: 
-            packet = new  CompactBlockCompleteMessage(); 
+          case common::BLOCK_COMPACT_COMPLETE_MESSAGE:
+            packet = new  CompactBlockCompleteMessage();
             break;
-          case common::READ_DATA_MESSAGE_V2: 
-            packet = new  ReadDataMessageV2(); 
+          case common::READ_DATA_MESSAGE_V2:
+            packet = new  ReadDataMessageV2();
             break;
-          case common::RESP_READ_DATA_MESSAGE_V2: 
-            packet = new  RespReadDataMessageV2(); 
+          case common::RESP_READ_DATA_MESSAGE_V2:
+            packet = new  RespReadDataMessageV2();
             break;
-          case common::LIST_BITMAP_MESSAGE: 
-            packet = new  ListBitMapMessage(); 
+          case common::LIST_BITMAP_MESSAGE:
+            packet = new  ListBitMapMessage();
             break;
-          case common::RESP_LIST_BITMAP_MESSAGE: 
-            packet = new  RespListBitMapMessage(); 
+          case common::RESP_LIST_BITMAP_MESSAGE:
+            packet = new  RespListBitMapMessage();
             break;
-          case common::RELOAD_CONFIG_MESSAGE: 
-            packet = new  ReloadConfigMessage(); 
+          case common::RELOAD_CONFIG_MESSAGE:
+            packet = new  ReloadConfigMessage();
             break;
-          case common::READ_RAW_DATA_MESSAGE: 
-            packet = new  ReadRawDataMessage(); 
+          case common::READ_RAW_DATA_MESSAGE:
+            packet = new  ReadRawDataMessage();
             break;
-          case common::RESP_READ_RAW_DATA_MESSAGE: 
-            packet = new  RespReadRawDataMessage(); 
+          case common::RESP_READ_RAW_DATA_MESSAGE:
+            packet = new  RespReadRawDataMessage();
             break;
-          case common::ACCESS_STAT_INFO_MESSAGE: 
-            packet = new  AccessStatInfoMessage(); 
+          case common::ACCESS_STAT_INFO_MESSAGE:
+            packet = new  AccessStatInfoMessage();
             break;
-          case common::READ_SCALE_IMAGE_MESSAGE: 
-            packet = new  ReadScaleImageMessage(); 
+          case common::READ_SCALE_IMAGE_MESSAGE:
+            packet = new  ReadScaleImageMessage();
             break;
-          case common::CRC_ERROR_MESSAGE: 
-            packet = new  CrcErrorMessage(); 
+          case common::CRC_ERROR_MESSAGE:
+            packet = new  CrcErrorMessage();
             break;
-          case common::OPLOG_SYNC_MESSAGE: 
-            packet = new  OpLogSyncMessage(); 
+          case common::OPLOG_SYNC_MESSAGE:
+            packet = new  OpLogSyncMessage();
             break;
-          case common::OPLOG_SYNC_RESPONSE_MESSAGE: 
-            packet = new  OpLogSyncResponeMessage(); 
+          case common::OPLOG_SYNC_RESPONSE_MESSAGE:
+            packet = new  OpLogSyncResponeMessage();
             break;
-          case common::MASTER_AND_SLAVE_HEART_MESSAGE: 
-            packet = new  MasterAndSlaveHeartMessage(); 
+          case common::MASTER_AND_SLAVE_HEART_MESSAGE:
+            packet = new  MasterAndSlaveHeartMessage();
             break;
-          case common::MASTER_AND_SLAVE_HEART_RESPONSE_MESSAGE: 
-            packet = new  MasterAndSlaveHeartResponseMessage(); 
+          case common::MASTER_AND_SLAVE_HEART_RESPONSE_MESSAGE:
+            packet = new  MasterAndSlaveHeartResponseMessage();
             break;
-          case common::HEARTBEAT_AND_NS_HEART_MESSAGE: 
-            packet = new  HeartBeatAndNSHeartMessage(); 
+          case common::HEARTBEAT_AND_NS_HEART_MESSAGE:
+            packet = new  HeartBeatAndNSHeartMessage();
             break;
-          case common::ADMIN_CMD_MESSAGE: 
-            packet = new  AdminCmdMessage(); 
+          case common::ADMIN_CMD_MESSAGE:
+            packet = new  AdminCmdMessage();
             break;
           case common::REMOVE_BLOCK_RESPONSE_MESSAGE:
             packet = new  RemoveBlockResponseMessage();
@@ -190,7 +190,25 @@ namespace tfs
             packet = new  DumpPlanResponseMessage();
             break;
           case common::SHOW_SERVER_INFORMATION_MESSAGE:
-            packet = new  ShowServerInformationMessage(); 
+            packet = new  ShowServerInformationMessage();
+            break;
+          case common::REQ_RC_LOGIN_MESSAGE:
+            packet = new ReqRcLoginMessage();
+            break;
+          case common::RSP_RC_LOGIN_MESSAGE:
+            packet = new RspRcLoginMessage();
+            break;
+          case common::REQ_RC_KEEPALIVE_MESSAGE:
+            packet = new ReqRcKeepAliveMessage();
+            break;
+          case common::RSP_RC_KEEPALIVE_MESSAGE:
+            packet = new RspRcKeepAliveMessage();
+            break;
+          case common::REQ_RC_LOGOUT_MESSAGE:
+            packet = new ReqRcLogoutMessage();
+            break;
+          case common::REQ_RC_RELOAD_MESSAGE:
+            packet = new ReqRcReloadMessage();
             break;
           default:
             TBSYS_LOG(ERROR, "pcode: %d not found in message factory", real_pcode);
