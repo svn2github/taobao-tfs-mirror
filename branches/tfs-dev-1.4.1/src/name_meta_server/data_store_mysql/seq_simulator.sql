3cfa4b6edd8aa169a5759ee0bd13053b70c06bd5
daoan@taobao.com <daoan@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-07-26 10:37:38 +0000

    change pid_seq_nextval so never return 0
    git-svn-id: http://code.taobao.org/svn/tfs@607 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-1.4.1/src/name_meta_server/data_store_mysql/seq_simulator.sql
+++ branches/tfs-dev-1.4.1/src/name_meta_server/data_store_mysql/seq_simulator.sql
@@ -5,20 +5,19 @@ CREATE TABLE  tfs_sequence (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into tfs_sequence (name, current_value) values ('seq_pid', 1);
+
 delimiter $$
 CREATE PROCEDURE  pid_seq_nextval()
 BEGIN
-  declare aff_row bigint;
   declare cur_val bigint;
   START TRANSACTION;
-  select current_value into cur_val from tfs_sequence where name = 'seq_pid';
   UPDATE tfs_sequence
-  SET current_value = current_value + increment where name = 'seq_pid' and current_value = cur_val;
-  select row_count() into aff_row;
+  SET current_value = current_value + increment where name = 'seq_pid';
+  select current_value - increment into cur_val from tfs_sequence where name = 'seq_pid';
   commit;
-  if aff_row <>1 then
-    select 0 into cur_val;
-  end if;
   select cur_val;
 END $$
 delimiter ;
+
+
+
