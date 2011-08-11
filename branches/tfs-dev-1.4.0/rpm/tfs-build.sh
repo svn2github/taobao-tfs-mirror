b584931c10ef82771dfb51b48ce6b3d63be441cd
duanfei@taobao.com <duanfei@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-08-11 02:33:19 +0000

    modify configure.ac
    git-svn-id: http://code.taobao.org/svn/tfs@699 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-1.4.0/rpm/tfs-build.sh
+++ branches/tfs-dev-1.4.0/rpm/tfs-build.sh
@@ -12,7 +12,7 @@ sed -i  "s/^Version:.*$/Version: "$3"/" $2.spec
 cd $temppath
 chmod +x build.sh
 ./build.sh init
-export TBLIB_ROOT=/usr
+export TBLIB_ROOT=/opt/csr/common
 ./configure
 make PREFIX=/home/admin/tfs rpms
 mv *.rpm rpm/
