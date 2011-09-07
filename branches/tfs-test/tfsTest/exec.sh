c2369609778d919ef8a5e12a52e32350f3d1a381
mingyan.zc@taobao.com <mingyan.zc@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-09-07 06:30:44 +0000

    modify compile script
    git-svn-id: http://code.taobao.org/svn/tfs@769 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-test/tfsTest/exec.sh
+++ branches/tfs-test/tfsTest/exec.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 LD_LIBRARY_PATH=/usr/local/staf/lib:${LD_LIBRARY_PATH}
-MAVEN_HOME=/home/admin/workspace/mingyan.zc/apache-maven-3.0.3
-JAVA_HOME=/usr/java/jdk1.6.0_20
+MAVEN_HOME=$(mvn -v | grep "Maven home" | awk '{print $3}')
+JAVA_HOME=$(mvn -v | grep "Java home" | awk '{print $3}')
 PATH=$MAVEN_HOME/bin:$JAVA_HOME:${PATH}
 CLASSPATH=/usr/local/staf/lib/JSTAF.jar:${CLASSPATH}
 export LD_LIBRARY_PATH MAVEN_HOME PATH JAVA_HOME CLASSPATH
