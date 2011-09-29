a63a279af7cd1a0ed1bdf324e677685ddbb812cc
duanfei@taobao.com <duanfei@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-09-29 05:28:20 +0000

    delete this directory
    git-svn-id: http://code.taobao.org/svn/tfs@869 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- trunk/scripts/ha/authkeys.sh
+++
@@ -1,5 +0,0 @@
-(echo -ne "auth 1\n1 sha1 ";dd if=/dev/urandom bs=512 count=1 | openssl md5) > authkeys
-cp -f ha.cf /etc/ha.d/
-cp -f authkeys /etc/ha.d/
-chmod 600 /etc/ha.d/authkeys
-chown root.root /etc/ha.d/authkeys
