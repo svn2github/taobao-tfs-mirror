91b8740ecc44b11c2d5f1d654250d71348adcdbb
daoan@taobao.com <daoan@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-10-21 09:02:04 +0000

    change meta_info to blob
    git-svn-id: http://code.taobao.org/svn/tfs@948 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-1.4.2/src/name_meta_server/data_store_mysql/create_table.sql
+++ branches/tfs-dev-1.4.2/src/name_meta_server/data_store_mysql/create_table.sql
@@ -8,6 +8,6 @@ CREATE TABLE t_meta_info (
   modify_time datetime not null,
   size bigint not null,
   ver_no smallint default 0,
-  meta_info text,
+  meta_info blob,
   PRIMARY KEY (app_id, uid, pid, name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
