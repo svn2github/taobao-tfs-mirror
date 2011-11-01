6b41b93db47e95dd9be9c803f7ff2e0b3302f33c
duanfei <duanfei@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-11-01 05:16:48 +0000

    fix bug, see http://rb.corp.taobao.com/r/17253/
    git-svn-id: http://code.taobao.org/svn/tfs@987 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-1.4.2/src/common/base_service.cpp
+++ branches/tfs-dev-1.4.2/src/common/base_service.cpp
@@ -113,6 +113,7 @@ namespace tfs
           if (!main_workers_.push(bpacket, work_queue_size_, false))
           {
             bpacket->reply_error_packet(TBSYS_LOG_LEVEL(ERROR),STATUS_MESSAGE_ERROR, "%s, task message beyond max queue size, discard", get_ip_addr());
+            bpacket->free();
           }
         }
       }
