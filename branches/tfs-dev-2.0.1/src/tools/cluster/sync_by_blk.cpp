cfe879f016f268757bd62fc98e02ef9e885b24bd
chuyu <chuyu@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2011-12-27 07:59:57 +0000

    fix interupt bug, see http://rb.corp.taobao.com/r/21375/
    git-svn-id: http://code.taobao.org/svn/tfs@1170 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-2.0.1/src/tools/cluster/sync_by_blk.cpp
+++ branches/tfs-dev-2.0.1/src/tools/cluster/sync_by_blk.cpp
@@ -62,12 +62,17 @@ struct LogFile
 
 tbutil::Mutex g_mutex_;
 SyncStat g_sync_stat_;
+static int32_t thread_count = 1;
+static const int32_t MAX_READ_LEN = 256;
+string src_ns_addr = "", dest_ns_addr = "";
+FILE *g_blk_done= NULL;
+FILE *g_file_succ = NULL, *g_file_fail = NULL;
 
 //int get_server_status();
 //int get_diff_block();
-int get_file_list(const string& src_ns_addr, const uint32_t block_id, BLOCK_FILE_INFO_VEC& v_block_file_info);
+int get_file_list(const string& ns_addr, const uint32_t block_id, BLOCK_FILE_INFO_VEC& v_block_file_info);
 int get_file_list(const string& ns_addr, const uint32_t block_id, FILE_NAME_VEC& v_file_name);
-int sync_file(const string& src_ns_addr, const string& dest_ns_addr, BLOCK_FILE_INFO_VEC& v_block_file_info, const bool force, const int32_t modify_time);
+int sync_file(const string& src_ns_addr, const string& dest_ns_addr, const uint32_t block_id, FileInfo& file_info, const bool force, const int32_t modify_time);
 
 class WorkThread : public tbutil::Thread
 {
@@ -100,22 +105,52 @@ class WorkThread : public tbutil::Thread
         BLOCK_ID_VEC_ITER iter = v_block_id_.begin();
         for (; iter != v_block_id_.end(); iter++)
         {
-          get_file_list(src_ns_addr_, (*iter), v_block_file_info);
-          sync_file(src_ns_addr_, dest_ns_addr_, v_block_file_info, force_, modify_time_);
-          if (need_remove_file_)
+          if (!destroy_)
           {
-            FILE_NAME_VEC src_v_file_name, dest_v_file_name, diff_v_file_name;
+            uint32_t block_id = (*iter);
+            bool block_done = false;
+            TBSYS_LOG(INFO, "sync block started. blockid: %u", block_id);
 
-            get_file_list(src_ns_addr_, (*iter), src_v_file_name);
-            TBSYS_LOG(DEBUG, "source file list size: %zd", src_v_file_name.size());
+            get_file_list(src_ns_addr_, block_id, v_block_file_info);
+            BLOCK_FILE_INFO_VEC_ITER file_info_iter = v_block_file_info.begin();
+            for (; file_info_iter != v_block_file_info.end(); file_info_iter++)
+            {
+              if (destroy_)
+              {
+                break;
+              }
+              sync_file(src_ns_addr_, dest_ns_addr_, block_id, (*file_info_iter).file_info_, force_, modify_time_);
+            }
+            block_done = (file_info_iter == v_block_file_info.end());
 
-            get_file_list(dest_ns_addr_, (*iter), dest_v_file_name);
-            TBSYS_LOG(DEBUG, "dest file list size: %zd", dest_v_file_name.size());
+            if (need_remove_file_)
+            {
+              block_done = false;
+              if (!destroy_)
+              {
+                FILE_NAME_VEC src_v_file_name, dest_v_file_name, diff_v_file_name;
 
-            get_diff_file_list(src_v_file_name, dest_v_file_name, diff_v_file_name);
-            TBSYS_LOG(DEBUG, "diff file list size: %zd", diff_v_file_name.size());
+                get_file_list(src_ns_addr_, block_id, src_v_file_name);
+                TBSYS_LOG(DEBUG, "source file list size: %zd", src_v_file_name.size());
 
-            unlink_file_list(diff_v_file_name);
+                get_file_list(dest_ns_addr_, block_id, dest_v_file_name);
+                TBSYS_LOG(DEBUG, "dest file list size: %zd", dest_v_file_name.size());
+
+                get_diff_file_list(src_v_file_name, dest_v_file_name, diff_v_file_name);
+                TBSYS_LOG(DEBUG, "diff file list size: %zd", diff_v_file_name.size());
+
+                unlink_file_list(diff_v_file_name);
+                TBSYS_LOG(DEBUG, "unlink file list size: %zd", diff_v_file_name.size());
+
+                block_done = true;
+              }
+            }
+
+            if (block_done)
+            {
+              fprintf(g_blk_done, "%u\n", block_id);
+              TBSYS_LOG(INFO, "sync block finished. blockid: %u", block_id);
+            }
           }
         }
       }
@@ -172,15 +207,12 @@ class WorkThread : public tbutil::Thread
 typedef tbutil::Handle<WorkThread> WorkThreadPtr;
 
 static WorkThreadPtr* gworks = NULL;
-static int32_t thread_count = 1;
-static const int32_t MAX_READ_LEN = 256;
-string src_ns_addr_ = "", dest_ns_addr_ = "";
-FILE *g_sync_succ = NULL, *g_sync_fail = NULL;
 
 struct LogFile g_log_fp[] =
 {
-  {&g_sync_succ, "sync_succ_file"},
-  {&g_sync_fail, "sync_fail_file"},
+  {&g_blk_done, "sync_done_blk"},
+  {&g_file_succ, "sync_succ_file"},
+  {&g_file_fail, "sync_fail_file"},
   {NULL, NULL}
 };
 
@@ -202,7 +234,7 @@ int init_log_file(char* dir_path)
 
 static void usage(const char* name)
 {
-  fprintf(stderr, "Usage: %s -s -d -b -e -u [-g] [-l] [-h]\n", name);
+  fprintf(stderr, "Usage: %s -s -d -f -e -u [-g] [-l] [-h]\n", name);
   fprintf(stderr, "       -s source ns ip port\n");
   fprintf(stderr, "       -d dest ns ip port\n");
   fprintf(stderr, "       -f block list file, assign a file to sync\n");
@@ -479,48 +511,44 @@ int get_file_list(const string& ns_addr, const uint32_t block_id, BLOCK_FILE_INF
 
   return ret;
 }
-int sync_file(const string& src_ns_addr, const string& dest_ns_addr, BLOCK_FILE_INFO_VEC& v_block_file_info, const bool force, const int32_t modify_time)
+int sync_file(const string& src_ns_addr, const string& dest_ns_addr, const uint32_t block_id, FileInfo& file_info,
+    const bool force, const int32_t modify_time)
 {
   int ret = TFS_SUCCESS;
-  UNUSED(force);
-  UNUSED(modify_time);
+
   SyncFileBase sync_op;
   sync_op.initialize(src_ns_addr, dest_ns_addr);
-  BLOCK_FILE_INFO_VEC_ITER iter = v_block_file_info.begin();
-  for (; iter != v_block_file_info.end(); iter++)
+  SyncAction sync_action;
+
+  // compare file info
+  ret = sync_op.cmp_file_info(block_id, file_info, sync_action, force, modify_time);
+
+  FSName fsname(block_id, file_info.id_, TfsClientImpl::Instance()->get_cluster_id(src_ns_addr.c_str()));
+  string file_name = string(fsname.get_name());
+  // do sync file
+  if (ret == TFS_SUCCESS)
   {
-    BlockFileInfo block_file_info = (*iter);
-    SyncAction sync_action;
-
-    // compare file info
-    ret = sync_op.cmp_file_info(block_file_info.block_id_, block_file_info.file_info_, sync_action, force, modify_time);
-
-    FSName fsname(block_file_info.block_id_, block_file_info.file_info_.id_, TfsClientImpl::Instance()->get_cluster_id(src_ns_addr.c_str()));
-    string file_name = string(fsname.get_name());
-    // do sync file
-    if (ret == TFS_SUCCESS)
-    {
-      ret = sync_op.do_action(file_name, sync_action);
-    }
-    if (ret == TFS_SUCCESS)
-    {
-      TBSYS_LOG(INFO, "sync file(%s) succeed.", file_name.c_str());
-      {
-        tbutil::Mutex::Lock lock(g_mutex_);
-        g_sync_stat_.success_count_++;
-      }
-      fprintf(g_sync_succ, "%s\n", file_name.c_str());
-    }
-    else
-    {
-      TBSYS_LOG(INFO, "sync file(%s) failed.", file_name.c_str());
-      {
-        tbutil::Mutex::Lock lock(g_mutex_);
-        g_sync_stat_.fail_count_++;
-      }
-      fprintf(g_sync_fail, "%s\n", file_name.c_str());
-    }
-    g_sync_stat_.actual_count_++;
+    ret = sync_op.do_action(file_name, sync_action);
   }
+  if (ret == TFS_SUCCESS)
+  {
+    TBSYS_LOG(INFO, "sync file(%s) succeed.", file_name.c_str());
+    {
+      tbutil::Mutex::Lock lock(g_mutex_);
+      g_sync_stat_.success_count_++;
+    }
+    fprintf(g_file_succ, "%s\n", file_name.c_str());
+  }
+  else
+  {
+    TBSYS_LOG(INFO, "sync file(%s) failed.", file_name.c_str());
+    {
+      tbutil::Mutex::Lock lock(g_mutex_);
+      g_sync_stat_.fail_count_++;
+    }
+    fprintf(g_file_fail, "%s\n", file_name.c_str());
+  }
+  g_sync_stat_.actual_count_++;
+
   return ret;
 }
