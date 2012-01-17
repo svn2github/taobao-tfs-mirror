b27b3ee08a61a9d487906769811911bf55b4c2a0
duanfei@taobao.com <duanfei@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-01-17 02:17:10 +0000

    fix bug, see http://rb.corp.taobao.com/r/22640/
    git-svn-id: http://code.taobao.org/svn/tfs@1200 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-dev-2.0.1/src/name_meta_server/lru.h
+++ branches/tfs-dev-2.0.1/src/name_meta_server/lru.h
@@ -16,17 +16,23 @@
 #ifndef TFS_NAMEMETASERVER_LRU_H_
 #define TFS_NAMEMETASERVER_LRU_H_
 
+#ifdef TFS_GTEST
+  #include <gtest/gtest.h>
+#endif
+
 #include <list>
 #include <ext/hash_map>
 #include "common/internal.h"
 #include "common/error_msg.h"
+#include "common/meta_hash_helper.h"
+#include "common/rts_define.h"
+
+#include "meta_cache_info.h"
 namespace tfs
 {
   namespace namemetaserver
   {
-    template<typename Key, typename Value>
     class BaseStrategy;
-    template<typename Key, typename Value>
     class Lru
     {
       #ifdef TFS_GTEST
@@ -37,107 +43,107 @@ namespace tfs
       FRIEND_TEST(LruTest, gc);
       #endif
 
-      template <typename T3>
-      struct Node;
+      typedef std::list<CacheRootNode*> LRU_LIST;
+      typedef LRU_LIST::iterator LRU_LIST_ITERATOR;
 
-      typedef std::list<Node<Value> > LRU_LIST;
-      typedef typename LRU_LIST::iterator LRU_LIST_ITERATOR;
+      typedef std::map<common::AppIdUid, LRU_LIST_ITERATOR> LRU_SUB_MAP;
+      typedef LRU_SUB_MAP::iterator LRU_SUB_MAP_ITERATOR;
 
-      //typedef __gnu_cxx::hash_map<Key, LRU_LIST_ITERATOR> LRU_MAP;
-      typedef std::map<Key, LRU_LIST_ITERATOR> LRU_MAP;
-      typedef typename LRU_MAP::iterator LRU_MAP_ITERATOR;
+      typedef std::map<int64_t, LRU_SUB_MAP>  LRU_MAP;
+      typedef LRU_MAP::iterator LRU_MAP_ITERATOR;
 
-      template <typename V>
-      struct Node
-      {
-        void inc_ref() { ++ref_count_;}
-        void dec_ref() { --ref_count_;}
-        void inc_visit_count() { ++visit_count_;}
-        V* value_;
-        int64_t ref_count_;
-        int64_t visit_count_;
-        LRU_MAP_ITERATOR iter_;
-      };
-
-      template<typename T, typename T2>
       friend class BaseStrategy;
     public:
       Lru(){}
       virtual ~Lru() {}
-      Value* get(const Key& key);
-      int put(const Key& key);
-      int insert(const Key& key, Value* value);
-      int gc(const double ratio, BaseStrategy<Key,Value>* st, std::vector<Value*>& values);
-      int gc(std::vector<Value*>& values);
-      int64_t size() { return index_.size();}
+      inline CacheRootNode* get(const common::AppIdUid& key);
+      inline int put(CacheRootNode* value);
+      inline int insert(const common::AppIdUid& key, CacheRootNode* value);
+      inline int gc(const double ratio, BaseStrategy* st, std::vector<CacheRootNode*>& values);
+      inline int gc(const std::set<int64_t>& buckets);
+      inline int gc(std::vector<CacheRootNode*>& values);
+      inline int64_t size();
     private:
-      Node<Value>* get_node(const Key& key);
+      inline CacheRootNode* get_node(const common::AppIdUid& key);
       LRU_LIST list_;
       LRU_MAP  index_;
       DISALLOW_COPY_AND_ASSIGN(Lru);
     };
 
-    template<typename Key, typename Value>
     class BaseStrategy
     {
     public:
-      explicit BaseStrategy(Lru<Key, Value>& lru): lru_(lru){}
+      explicit BaseStrategy(Lru& lru): lru_(lru){}
       virtual ~BaseStrategy(){}
-      virtual int gc(const double ratio, std::vector<Value*>& values);
+      inline virtual int gc(const double ratio, std::vector<CacheRootNode*>& values);
     private:
-      int64_t calc_gc_count(const double ratio);
-      Lru<Key, Value>& lru_;
+      inline int64_t calc_gc_count(const double ratio);
+      Lru& lru_;
     };
 
-    template <typename Key, typename Value>
-    Value* Lru<Key, Value>::get(const Key& key)
+    CacheRootNode* Lru::get(const common::AppIdUid& key)
     {
-      Value* value = NULL;
-      LRU_MAP_ITERATOR iter = index_.find(key);
+      CacheRootNode* value = NULL;
+      int64_t hash_value = key.get_hash();
+      LRU_MAP_ITERATOR iter = index_.find(hash_value);
       if (index_.end() != iter)
       {
-        list_.splice(list_.end(), list_, iter->second);
-        iter->second->inc_visit_count();
-        iter->second->inc_ref();
-        TBSYS_LOG(DEBUG, "ref_count_ = %l"PRI64_PREFIX"d", iter->second->ref_count_);
-        value = iter->second->value_;
+        LRU_SUB_MAP_ITERATOR sub_iter = iter->second.find(key);
+        if (iter->second.end() != sub_iter)
+        {
+          list_.splice(list_.end(), list_, sub_iter->second);
+          (*sub_iter->second)->inc_visit_count();
+          (*sub_iter->second)->inc_ref();
+          value = (*sub_iter->second);
+          TBSYS_LOG(DEBUG, "ref_count_ = %l"PRI64_PREFIX"d", (*sub_iter->second)->ref_count_);
+        }
       }
       return value;
     }
 
-    template<typename Key, typename Value>
-    int Lru<Key, Value>::put(const Key& key)
+    int Lru::put(CacheRootNode* value)
     {
-      LRU_MAP_ITERATOR iter = index_.find(key);
-      int32_t iret = index_.end() == iter ? common::EXIT_LRU_VALUE_NOT_EXIST : common::TFS_SUCCESS;
+      int32_t iret = NULL == value ? common::EXIT_PARAMETER_ERROR : common::TFS_SUCCESS;
       if (common::TFS_SUCCESS == iret)
       {
-        iter->second->dec_ref();
-        TBSYS_LOG(DEBUG, "ref_count_ = %l"PRI64_PREFIX"d", iter->second->ref_count_);
+        value->dec_ref();
       }
       return iret;
     }
 
-    template<typename Key, typename Value>
-    int Lru<Key, Value>::insert(const Key& key, Value* value)
+    int Lru::insert(const common::AppIdUid& key, CacheRootNode* value)
     {
-      LRU_MAP_ITERATOR iter = index_.find(key);
-      int32_t iret = index_.end() != iter ? common::EXIT_LRU_VALUE_EXIST : common::TFS_SUCCESS;
+      int32_t iret = NULL == value ? common::EXIT_PARAMETER_ERROR : common::TFS_SUCCESS;
       if (common::TFS_SUCCESS == iret)
       {
-        LRU_LIST_ITERATOR it = list_.insert(list_.end(), Node<Value>());
-        std::pair<LRU_MAP_ITERATOR, bool> res = index_.insert(std::pair<Key, LRU_LIST_ITERATOR>(key, it));
-        iret = res.second ? common::TFS_SUCCESS : common::EXIT_LRU_VALUE_EXIST;
-        it->value_ = value;
-        it->ref_count_ = 0;
-        it->visit_count_ = 0;
-        it->iter_ = res.first;
+        int64_t hash_value = key.get_hash();
+        LRU_MAP_ITERATOR iter = index_.find(hash_value);
+        if (index_.end() == iter)
+        {
+          std::pair<LRU_MAP_ITERATOR, bool> res1 = index_.insert(std::pair<int64_t, LRU_SUB_MAP>(
+            hash_value, std::map<common::AppIdUid, LRU_LIST_ITERATOR>()));
+          iter = res1.first;
+        }
+
+        LRU_SUB_MAP_ITERATOR sub_iter = iter->second.find(key);
+        iret = iter->second.end() != sub_iter ? common::EXIT_LRU_VALUE_EXIST : common::TFS_SUCCESS;
+        if (common::TFS_SUCCESS == iret)
+        {
+          value->ref_count_   = 0;
+          value->visit_count_ = 0;
+          LRU_LIST_ITERATOR it = list_.insert(list_.end(), value);
+          std::pair<LRU_SUB_MAP_ITERATOR, bool> res = iter->second.insert(std::pair<common::AppIdUid, LRU_LIST_ITERATOR>(key, it));
+          iret = res.second ? common::TFS_SUCCESS : common::EXIT_LRU_VALUE_EXIST;
+          if (common::TFS_SUCCESS != iret)
+          {
+            list_.pop_back();
+          }
+        }
       }
       return iret;
     }
 
-    template<typename Key, typename Value>
-    int Lru<Key, Value>::gc(const double ratio, BaseStrategy<Key,Value>* st, std::vector<Value*>& values)
+    int Lru::gc(const double ratio, BaseStrategy* st, std::vector<CacheRootNode*>& values)
     {
       int32_t iret = NULL == st ? common::TFS_ERROR : common::TFS_SUCCESS;
       if (common::TFS_SUCCESS == iret)
@@ -147,43 +153,86 @@ namespace tfs
       return iret;
     }
 
-    template<typename Key, typename Value>
-    int Lru<Key, Value>::gc(std::vector<Value*>& values)
+    int Lru::gc(std::vector<CacheRootNode*>& values)
     {
       LRU_LIST_ITERATOR iter = list_.begin();
       for (; iter != list_.end(); ++iter)
       {
-        values.push_back(iter->value_);
+        values.push_back((*iter));
       }
       list_.clear();
       index_.clear();
       return common::TFS_SUCCESS;
     }
 
-    template<typename Key, typename Value>
-    Lru<Key, Value>::Node<Value>* Lru<Key, Value>::get_node(const Key& key)
+    int Lru::gc(const std::set<int64_t>& buckets)
     {
-      LRU_MAP_ITERATOR iter = index_.find(key);
-      return index_.end() != iter ? (&(*iter->second)) : NULL;
+      std::set<int64_t>::const_iterator iter = buckets.begin();
+      for (; iter != buckets.end(); ++iter)
+      {
+        LRU_MAP_ITERATOR it = index_.find((*iter));
+        if (index_.end() != it)
+        {
+          index_.erase(it);
+        }
+      }
+      return common::TFS_SUCCESS;
     }
 
-    template<typename Key, typename Value>
-    int BaseStrategy<Key, Value>::gc(const double ratio, std::vector<Value*>& values)
+    int64_t Lru::size()
+    {
+      int64_t count = 0;
+      LRU_MAP_ITERATOR iter = index_.begin();
+      for (; iter != index_.end(); ++iter)
+      {
+        count += iter->second.size();
+      }
+      return count;
+    }
+
+    CacheRootNode* Lru::get_node(const common::AppIdUid& key)
+    {
+      CacheRootNode* value = NULL;
+      int64_t hash_value = key.get_hash();
+      LRU_MAP_ITERATOR iter = index_.find(hash_value);
+      if (index_.end() != iter)
+      {
+        LRU_SUB_MAP_ITERATOR sub_iter = iter->second.find(key);
+        value = iter->second.end() != sub_iter ? (*sub_iter->second) : NULL;
+      }
+      return value;
+    }
+
+    int BaseStrategy::gc(const double ratio, std::vector<CacheRootNode*>& values)
     {
       int64_t begin = lru_.size() - calc_gc_count(ratio);
+      if (begin < 0)
+          begin = 0;
       int32_t iret = begin >= 0 ? common::TFS_SUCCESS : common::TFS_ERROR;
       if (common::TFS_SUCCESS == iret)
       {
         int64_t gc_count = calc_gc_count(ratio);
         int64_t count = 0;
-        typename Lru<Key, Value>::LRU_LIST_ITERATOR iter = lru_.list_.begin();
+        Lru::LRU_LIST_ITERATOR iter = lru_.list_.begin();
         for(; iter != lru_.list_.end() && count < gc_count;)
         {
-          if (0 == iter->ref_count_)
+          if ((*iter)->ref_count_ <= 0)
           {
             ++count;
-            values.push_back(iter->value_);
-            lru_.index_.erase(iter->iter_);
+            values.push_back((*iter));
+            int64_t hash_value = (*iter)->get_hash();
+            Lru::LRU_MAP_ITERATOR it = lru_.index_.find(hash_value);
+            if (lru_.index_.end() != it)
+            {
+              Lru::LRU_SUB_MAP_ITERATOR sub_iter = it->second.find((*iter)->key_);
+              if (it->second.end() != sub_iter)
+              {
+                if ((*iter) == (*sub_iter->second))
+                {
+                  it->second.erase(sub_iter);
+                }
+              }
+            }
             lru_.list_.erase(iter++);
           }
           else
@@ -195,8 +244,7 @@ namespace tfs
       return iret;
     }
 
-    template<typename Key, typename Value>
-    int64_t BaseStrategy<Key, Value>::calc_gc_count(const double ratio)
+    int64_t BaseStrategy::calc_gc_count(const double ratio)
     {
       return static_cast<int64_t>(lru_.size() * ratio) + 1;
     }
