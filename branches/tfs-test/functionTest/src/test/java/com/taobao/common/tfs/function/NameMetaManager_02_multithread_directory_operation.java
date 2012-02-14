e23d7287acece4c04c6aac33de44768943c17a07
lexin <lexin@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-02-14 12:05:33 +0000

    create function test case branch
    git-svn-id: http://code.taobao.org/svn/tfs@1228 b722c2b5-6a69-4643-bcd1-1a75825fa998

---
+++ branches/tfs-test/functionTest/src/test/java/com/taobao/common/tfs/function/NameMetaManager_02_multithread_directory_operation.java
@@ -0,0 +1,294 @@
+package com.taobao.common.tfs.function;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner; 
+import net.sourceforge.groboutils.junit.v1.TestRunnable;  
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.taobao.common.tfs.NameMetaManagerBaseCase;
+import com.taobao.common.tfs.namemeta.FileMetaInfo;
+
+public class NameMetaManager_02_multithread_directory_operation extends  NameMetaManagerBaseCase{
+
+  public static String rootDir = "/NameMetaTest2";
+
+  @BeforeClass
+  public  static void setUpOnce() throws Exception {
+    //rmDirRecursive(appId, userId, rootDir); 
+    tfsManager.createDir(appId, userId, rootDir); 
+  }
+
+  @AfterClass
+  public static void tearDownOnce() throws Exception {
+    rmDirRecursive(appId, userId, rootDir); 
+  }
+
+
+	//@Test
+	public void test_01_multi_threads_create_different_directories() throws Throwable {
+		TestRunnable tr1, tr2, tr3, tr4, tr5, tr6;
+		tr1 = new ThreadCreateDir();
+		tr2 = new ThreadCreateDir();
+		tr3 = new ThreadCreateDir();
+		tr4 = new ThreadCreateDir();
+		tr5 = new ThreadCreateDir();
+		tr6 = new ThreadCreateDir();
+
+		TestRunnable[] trs = {tr1, tr2, tr3, tr4, tr5, tr6};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+	
+	//@Test
+	public void test_02_multi_threads_rename_differernt_directories_in_same_dir() throws Throwable {
+		TestRunnable tr1, tr2, tr3;
+		tr1 = new ThreadRenameDirs();
+		tr2 = new ThreadRenameDirs();
+		tr3 = new ThreadRenameDirs();
+
+		TestRunnable[] trs = {tr1, tr2, tr3};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+	
+	//@Test
+	public void test_03_multi_threads_move_differernt_directories_in_same_dir() throws Throwable {
+		TestRunnable tr1, tr2, tr3;
+		tr1 = new ThreadMoveDirs();
+		tr2 = new ThreadMoveDirs();
+		tr3 = new ThreadMoveDirs();
+
+		TestRunnable[] trs = {tr1, tr2, tr3};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+  
+	//@Test
+	public void test_04_multi_threads_delete_different_directories() throws Throwable {
+		TestRunnable tr1, tr2, tr3, tr4, tr5, tr6;
+		tr1 = new ThreadRmDirs();
+		tr2 = new ThreadRmDirs();
+		tr3 = new ThreadRmDirs();
+		tr4 = new ThreadRmDirs();
+		tr5 = new ThreadRmDirs();
+		tr6 = new ThreadRmDirs();
+
+		TestRunnable[] trs = {tr1, tr2, tr3, tr4, tr5, tr6};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+    
+	//@Test
+	public void test_05_multi_threads_create_and_delete_one_directory() throws Throwable {
+		TestRunnable tr1, tr2;
+		tr1 = new ThreadCreateRmDir();
+		tr2 = new ThreadCreateRmDir();
+	
+		TestRunnable[] trs = {tr1, tr2};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+    
+	@Test
+	public void test_06_multi_threads_move_one_directory() throws Throwable {
+		TestRunnable tr1, tr2;
+		tr1 = new ThreadMoveOneDir();
+		tr2 = new ThreadMoveOneDir();
+	
+		TestRunnable[] trs = {tr1, tr2};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+	
+	@Test
+	public void test_07_multi_threads_rename_one_directory() throws Throwable {
+		TestRunnable tr1, tr2;
+		tr1 = new ThreadRenameOneDir();
+		tr2 = new ThreadRenameOneDir();
+	
+		TestRunnable[] trs = {tr1, tr2};
+		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
+		mttr.runTestRunnables();
+	}
+	
+	//Threads for every test case to call
+  private class ThreadCreateDir extends TestRunnable {
+      public long count = 1000;
+      @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+        while (--count > 0) {
+          boolean ret = false;
+          ret = tfsManager.createDir(appId, userId, rootDir + "/" + Thread.currentThread().getId() + "/" + String.valueOf(count));
+          Assert.assertTrue(ret);
+        }
+      }
+  }
+
+  private class ThreadRenameDirs extends TestRunnable {
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        rename_different_directories();
+      }
+  }
+
+  private class ThreadMoveDirs extends TestRunnable {
+
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        move_different_directories();
+      }
+  }
+  private class ThreadRmDirs extends TestRunnable {
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        delete_different_directories();
+      }
+  }
+  private class ThreadCreateRmDir extends TestRunnable {
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        create_and_delete_one_directory();
+      }
+  }
+  private class ThreadMoveOneDir extends TestRunnable {
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        move_one_directory();
+      }
+  }
+  private class ThreadRenameOneDir extends TestRunnable {
+    @Override
+      public void runTest() throws Throwable {
+        //线程要调用的方法或者要执行的操作
+        rename_one_directory();
+      }
+  }
+
+  public void create_and_delete_one_directory() throws Exception 
+  {
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+
+    String filepath = rootDir + "/public";
+    String subDir = filepath + "/subdir";
+    String suberDir = subDir + "/suberdir";
+
+    tfsManager.createDir(appId, userId, filepath);
+
+    tfsManager.createDir(appId, userId, subDir);
+
+    tfsManager.createDir(appId, userId, suberDir);
+
+    tfsManager.rmDir(appId, userId, suberDir);
+
+    tfsManager.rmDir(appId, userId, subDir);
+
+    tfsManager.rmDir(appId, userId, filepath);
+  }
+
+  public void rename_different_directories() throws Exception {
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+
+    boolean ret = false;
+    long threadId = Thread.currentThread().getId();
+    String filepath = rootDir + "/" + String.valueOf(threadId);
+    String subDir = filepath + "/subdir";
+    String renamedDir = filepath + "/renamed";
+
+    ret = tfsManager.createDir(appId, userId, filepath);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.createDir(appId, userId, subDir);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.mvDir(appId, userId, subDir, renamedDir);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.rmDir(appId, userId, renamedDir);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.rmDir(appId, userId, filepath);
+    Assert.assertTrue(ret);
+  }
+
+  public void rename_one_directory() throws Exception 
+  {    	 
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+    String filepath = rootDir + "/public/test";
+    String newpath = rootDir + "/public/renamed";
+
+    tfsManager.createDir(appId, userId, filepath);
+
+    tfsManager.mvDir(appId, userId, filepath, newpath);
+
+    tfsManager.rmDir(appId, userId, newpath);
+  }
+
+  public void move_different_directories() throws Exception 
+  {    	 
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+
+    boolean ret = false;
+    long threadId = Thread.currentThread().getId();
+    String filepath = rootDir + "/" + String.valueOf(threadId);
+    String newpath = rootDir + "/moved_" + String.valueOf(threadId);
+
+    ret = tfsManager.createDir(appId, userId, filepath);
+    Assert.assertTrue(ret);
+
+    ret= tfsManager.mvDir(appId, userId, filepath, newpath);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.rmDir(appId, userId, newpath);
+    Assert.assertTrue(ret);   
+  }
+
+  public void move_one_directory() throws Exception 
+  {    	 
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+
+    boolean ret = false;
+    String filepath = rootDir + "/public/test";
+    String newpath = rootDir + "/moved";
+
+    ret = tfsManager.createDir(appId, userId, filepath);
+
+    ret=  tfsManager.mvDir(appId, userId, filepath, newpath);
+
+    ret = tfsManager.rmDir(appId, userId, newpath);
+  }
+
+  public void delete_different_directories() throws Exception 
+  {    	 
+    log.info("线程===" + Thread.currentThread().getId() + "执行开始");
+
+    boolean ret = false;
+    long threadId = Thread.currentThread().getId();
+
+    String filepath = rootDir + "/" + String.valueOf(threadId);
+
+    ret = tfsManager.createDir(appId, userId, filepath);
+    Assert.assertTrue(ret);
+
+    ret = tfsManager.rmDir(appId, userId, filepath);
+    Assert.assertTrue(ret);
+  }
+}
---
+++ branches/tfs-test/functionTest/src/test/java/com/taobao/common/tfs/function/mergeRcMetaManager_10_nodes_block.java
@@ -0,0 +1,282 @@
+/**
+ * 
+ */
+package com.taobao.common.tfs.function;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.taobao.gaia.KillTypeEnum;
+import com.taobao.common.tfs.DefaultTfsManager; 
+import com.taobao.common.tfs.NameMetaBaseCase;
+
+
+/**
+ * @author Administrator/chuyu
+ *
+ */
+public class mergeRcMetaManager_10_nodes_block extends NameMetaBaseCase {
+
+  public long appId = 3;
+  public long userId = 2711;
+  public int CHECK_TIME = 60;
+  public int MIN_ERROR_TIME = 0; // empiric value
+  public int MAX_ERROR_TIME = 30; // empiric value
+  public int LEASE_TIME = 15; // the same to rootserver lease time
+  public int DIFF_TIME = 15; // empiric value, Error Threshold
+  @Before
+    public void setUp(){
+
+    }
+
+  @After
+    public void tearDown(){
+    }
+
+  @Test
+    public void Function_00_block_unserveringMs_and_rs() {
+
+      caseName = "Function_00_block_unserveringMs_and_rs";
+      log.info(caseName + "===> start");
+
+      // test client error time
+      long currentTime = 0;
+      long currentRowNum = 0;
+      long failStartTime = 0;
+      long failEndTime = 0;
+      // test rootserver version
+      long oldVersion = 0;
+      long newVersion = 0;
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+      mixOpCmd();
+      sleep(WAIT_TIME);
+      int msIndex = getUnServingMSIndex(appId, userId);
+      log.debug(" @@ msIndex: " + msIndex);
+      Assert.assertTrue(msIndex > -1);
+
+      // block server
+
+      oldVersion = getRsCurrentVersion();
+      log.debug(" @@ oldVersion: " + oldVersion);
+
+      fullBlockMetaServerAndRS(msIndex);
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(30);
+     
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      //Assert.assertTrue(1 == (newVersion - oldVersion));
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      //Assert.assertTrue(failStartTime - currentTime < LEASE_TIME);
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      //Assert.assertTrue(failEndTime - failStartTime < MAX_ERROR_TIME);
+      //Assert.assertTrue(failEndTime - failStartTime >= MIN_ERROR_TIME);
+
+      // unblock server
+      oldVersion = newVersion;
+
+      unblockMetaServerToRS(msIndex);
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(CHECK_TIME);
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      //Assert.assertTrue(failStartTime - currentTime < LEASE_TIME);
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      //Assert.assertTrue(failEndTime - failStartTime < MAX_ERROR_TIME);
+      //Assert.assertTrue(failEndTime - failStartTime >= MIN_ERROR_TIME);
+
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      //Assert.assertTrue(1 <= newVersion - oldVersion); // metaserver may send packets to rootserver
+
+      mixOpCmdStop();
+    } 
+
+  @Ignore
+    public void Function_01_block_serveringMs_and_rs() {
+
+      caseName = "Function_01_block_serveringMs_and_rs";
+      log.info(caseName + "===> start");
+
+      // test client error time
+      long currentTime = 0;
+      long currentRowNum = 0;
+      long failStartTime = 0;
+      long failEndTime = 0;
+      // test rootserver version
+      long oldVersion = 0;
+      long newVersion = 0;
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+      mixOpCmd();
+      sleep(WAIT_TIME);
+      int msIndex = getServingMSIndex(appId, userId);
+      log.debug(" @@ msIndex: " + msIndex);
+      Assert.assertTrue(msIndex > -1);
+
+      // block server
+
+      oldVersion = getRsCurrentVersion();
+      log.debug(" @@ oldVersion: " + oldVersion);
+
+      fullBlockMetaServerAndRS(msIndex);
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(CHECK_TIME);
+     
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      Assert.assertTrue(1 == (newVersion - oldVersion));
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      Assert.assertTrue(failStartTime - currentTime < LEASE_TIME);
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      Assert.assertTrue(failEndTime - failStartTime < MAX_ERROR_TIME);
+      Assert.assertTrue(failEndTime - failStartTime >= MIN_ERROR_TIME);
+
+      // unblock server
+      oldVersion = newVersion;
+
+      unblockMetaServerToRS(msIndex);
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(CHECK_TIME);
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      Assert.assertTrue(failStartTime - currentTime < LEASE_TIME);
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      Assert.assertTrue(failEndTime - failStartTime < MAX_ERROR_TIME);
+      Assert.assertTrue(failEndTime - failStartTime >= MIN_ERROR_TIME);
+
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      Assert.assertTrue(1 <= newVersion - oldVersion); // metaserver may send packets to rootserver
+
+      mixOpCmdStop();
+    } 
+
+  @Ignore
+    public void Function_02_block_serveringMs_and_client() {
+
+      caseName = "Function_02_block_serveringMs_and_client";
+      log.info(caseName + "===> start");
+
+      // test client error time
+      long currentTime = 0;
+      long currentRowNum = 0;
+      long failStartTime = 0;
+      long failEndTime = 0;
+      // test rootserver version
+      long oldVersion = 0;
+      long newVersion = 0;
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+      mixOpCmd();
+      sleep(WAIT_TIME);
+      int msIndex = getServingMSIndex(appId, userId);
+      log.debug(" @@ msIndex: " + msIndex);
+      Assert.assertTrue(msIndex > -1);
+
+      // block server
+
+      oldVersion = getRsCurrentVersion();
+      log.debug(" @@ oldVersion: " + oldVersion);
+
+      fullBlockClientrToMS(msIndex);
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(CHECK_TIME);
+     
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      Assert.assertTrue(newVersion == oldVersion);
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      Assert.assertTrue(failEndTime - failStartTime < CHECK_TIME + DIFF_TIME);
+      Assert.assertTrue(failEndTime - failStartTime > CHECK_TIME - DIFF_TIME);
+
+      // unblock server
+      oldVersion = newVersion;
+
+      unblockClientToMS();
+
+      currentTime = getClientCurrentTime();
+      log.debug(" @@ currentTime: " + df.format(new java.util.Date(currentTime * 1000)));
+      currentRowNum = getClientCurrentRowNum();
+      log.debug(" @@ currentRow: " + currentRowNum);
+
+      sleep(CHECK_TIME);
+
+      newVersion = getRsCurrentVersion();
+      log.debug(" @@ newVersion: " + newVersion);
+      Assert.assertTrue(newVersion == oldVersion);
+
+      failStartTime = getFailStartTime(currentRowNum);
+      failEndTime = getFailEndTime(currentRowNum);
+      // server can still do service in lease time
+      log.debug("operation start faied between " + df.format(new java.util.Date(currentTime * 1000)) + " and " + df.format(new java.util.Date(failStartTime * 1000)));
+      //should check log, maybe sometimes error not occured by update rootserver tables
+      log.debug("operation faied between " + df.format(new java.util.Date(failStartTime * 1000)) + " and " + df.format(new java.util.Date(failEndTime * 1000)));
+      Assert.assertTrue(failStartTime == 0);
+      Assert.assertTrue(failEndTime == 0);
+
+      mixOpCmdStop();
+    } 
+}
