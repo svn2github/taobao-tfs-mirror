7a314adea325728064504ba2f4c6e4b19dfe5e1c
lexin <lexin@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-02-29 08:45:44 +0000

    git-svn-id: http://code.taobao.org/svn/tfs@1296 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-test/interfaceTest/src/test/java/com/taobao/common/tfs/rcitest/RcTfsManager_05_fetchFile.java
+++ branches/tfs-test/interfaceTest/src/test/java/com/taobao/common/tfs/rcitest/RcTfsManager_05_fetchFile.java
@@ -183,7 +183,7 @@ public class RcTfsManager_05_fetchFile extends tfsNameBaseCase
 		int fetchcrc;
 		boolean Bret;
 		
-		Ret=tfsManager.saveFile( resourcesPath+"100K.jpg",null,".jpg",false);
+		Ret=tfsManager.saveFile( resourcesPath+"100k.jpg",null,".jpg",false);
 	
 		savecrc=getCrc(resourcesPath+"100K.jpg");
 		Assert.assertNotNull(Ret);
@@ -197,11 +197,11 @@ public class RcTfsManager_05_fetchFile extends tfsNameBaseCase
 		Bret=tfsManager.unlinkFile(Ret, null);
 		Assert.assertTrue(Bret);
 		
-		Ret=tfsManager.saveFile( resourcesPath+"100K.jpg",name,null,true);
+		Ret=tfsManager.saveFile( resourcesPath+"100k.jpg",name,null,true);
 		System.out.println("The tfs file 2 name is "+ Ret);
 		
 		
-		Bret=tfsManager.fetchFile(Ret, null, resourcesPath+"TEMP");
+		Bret=tfsManager.fetchFile(Ret, ".jpg", resourcesPath+"TEMP");
 		Assert.assertTrue(Bret);
 		
 		fetchcrc=getCrc(resourcesPath+"TEMP");
