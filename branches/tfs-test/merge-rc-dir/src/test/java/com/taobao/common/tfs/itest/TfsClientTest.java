c9aee14128e7a915d53c82f7ff5d3134643c4ab0
yiming <yiming@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-03-08 06:10:30 +0000

    git-svn-id: http://code.taobao.org/svn/tfs@1337 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-test/merge-rc-dir/src/test/java/com/taobao/common/tfs/itest/TfsClientTest.java
+++ branches/tfs-test/merge-rc-dir/src/test/java/com/taobao/common/tfs/itest/TfsClientTest.java
@@ -41,6 +41,7 @@ public class TfsClientTest
 
         public void setFileName(String fileName) {
             this.fileName = fileName;
+            /////
         }
         public String getFileName() {
             return fileName;
