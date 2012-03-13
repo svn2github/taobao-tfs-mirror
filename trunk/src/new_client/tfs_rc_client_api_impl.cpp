749805a4dbcc79aabc77790ac5ecd64450691a70
mingyan.zc@taobao.com <mingyan.zc@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-03-13 01:35:08 +0000

    fix bug. see http://rb.corp.taobao.com/r/25646/
    git-svn-id: http://code.taobao.org/svn/tfs@1344 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- trunk/src/new_client/tfs_rc_client_api_impl.cpp
+++ trunk/src/new_client/tfs_rc_client_api_impl.cpp
@@ -823,7 +823,7 @@ namespace tfs
         string ns_addr;
         do
         {
-          ns_addr = get_ns_addr(NULL, RcClient::READ, ns_get_index++);
+          ns_addr = get_ns_addr(file_name, RcClient::READ, ns_get_index++);
           if (ns_addr.empty())
           {
             break;
@@ -844,7 +844,7 @@ namespace tfs
         string ns_addr;
         do
         {
-          ns_addr = get_ns_addr(NULL, RcClient::READ, ns_get_index++);
+          ns_addr = get_ns_addr(file_name, RcClient::READ, ns_get_index++);
           if (ns_addr.empty())
           {
             break;
