169953fede4e7ceef5fa473e224e118d3922ad9e
yinshuang <yinshuang@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2012-05-17 09:59:07 +0000

    add server implementation
    git-svn-id: http://code.taobao.org/svn/tfs@1458 b722c2b5-6a69-4643-bcd1-1a75825fa998

---
+++ branches/tfs-test/V2/src/main/java/com/taobao/common/tfs/server/DataServer.java
@@ -0,0 +1,17 @@
+package com.taobao.common.tfs.server;
+
+import com.taobao.common.tfs.config.DataServerConfig;
+
+public class DataServer extends DefaultTfsServer {
+	private DataServerConfig configuration;
+	
+	public void setConfiguration(DataServerConfig configuration){
+		this.configuration = configuration;
+	}
+	
+	@Override
+	public DataServerConfig getConfiguration() {
+		return configuration;
+	}
+
+}
