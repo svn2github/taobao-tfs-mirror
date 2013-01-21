4a8e8bcee8aa3aea1a790a7485ba25699f48d274
diqing <diqing@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2013-01-21 09:12:54 +0000

    注释 ds cache 相关case
    git-svn-id: http://code.taobao.org/svn/tfs@1920 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/tfs-test/V2/src/test/java/com/taobao/common/tfs/testcase/function/ds/SmallFileCacheTest.java
+++ branches/tfs-test/V2/src/test/java/com/taobao/common/tfs/testcase/function/ds/SmallFileCacheTest.java
@@ -1,165 +1,165 @@
-package com.taobao.common.tfs.testcase.function.ds;
-
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import com.taobao.common.tfs.testcase.rcTfsBaseCase;
-import com.taobao.common.tfs.utility.FileUtility;
-
-public class SmallFileCacheTest extends rcTfsBaseCase
-{
-
-  
-    
-	
-    @Test
-    public void localExsitRemoteExistSamllFile() throws Exception 
-    {
-        boolean bRet = false;
-        OutputStream output = new FileOutputStream("tmp");
-        byte [] data = FileUtility.getByte(localFile);
-
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
-        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
-        
-        String sRet;
-        /* set local remote cache */
-        tfsManager.setEnableLocalCache(true);
-        tfsManager.setEnableRemoteCache(true);
-        
-        /* Write file */
-        sRet = tfsManager.saveFile(data, null, null);
-        Assert.assertNotNull(sRet);
-        /* Read file */   
-        tfsManager.removeLocalCache(sRet);
-        tfsManager.removeRemoteCache(sRet);
-        Assert.assertFalse(tfsManager.isHitLocalCache(sRet)); 
-        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet)); 
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        
-     
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-         
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet)); 
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
-    }
-    @Test
-    public void localExistRemoteNonexsitentSamllFile() throws Exception 
-    {
-        boolean bRet = false;
-        OutputStream output = new FileOutputStream("tmp");
-        byte [] data = FileUtility.getByte(this.localFile);
-
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
-        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
-        
-        String sRet;
-        /* set  cache */
-        tfsManager.setEnableLocalCache(true);
-        tfsManager.setEnableRemoteCache(true);
-        
-        /* Write file */
-        sRet = tfsManager.saveFile(data, null, null);
-        Assert.assertNotNull(sRet);
-        /* Read file */     
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        //remove remote cache 
-        tfsManager.removeRemoteCache(sRet);
-        
-        //read file  form local cache
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
-        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
-        
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
-    }
-    @Test
-    public void localNonexsitentRemoteNonexsitentSamllFile() throws Exception 
-    {
-        boolean bRet = false;
-        OutputStream output = new FileOutputStream("tmp");
-        byte [] data = FileUtility.getByte(this.localFile);
-
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
-        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
-        
-        String sRet;
-        /* set local cache */
-        tfsManager.setEnableLocalCache(true);
-        tfsManager.setEnableRemoteCache(true);
-        
-        /* Write file */
-        sRet = tfsManager.saveFile(data, null, null);
-        Assert.assertNotNull(sRet); 
-        /* Read file */     
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp"));        
-        
-        //remove remote local  cache 
-        tfsManager.removeRemoteCache(sRet);
-        tfsManager.removeLocalCache(sRet);
-        Assert.assertFalse(tfsManager.isHitLocalCache(sRet));
-        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
-        //read file  form ds
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        //tfsManager.destroy();
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
-    }
-    @Test
-    public void localNonexsitentRemoteExistSamllFile() throws Exception 
-    {
-        boolean bRet = false;
-        OutputStream output = new FileOutputStream("tmp");
-        byte [] data = FileUtility.getByte(this.localFile);
-
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
-        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);      
-        String sRet;
-        /* Write file */
-        sRet = tfsManager.saveFile(data, null, null);
-        Assert.assertNotNull(sRet);
-         
-        /* set local remote cache */
-        tfsManager.setEnableLocalCache(true);
-        tfsManager.setEnableRemoteCache(true);
-        
-        /* Read file */     
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        
-        
-        //remove  local  cache 
-        tfsManager.removeLocalCache(sRet);
-        //read file  form ds
-        bRet = tfsManager.fetchFile(sRet, null, output);
-        Assert.assertTrue(bRet);
-        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
-        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
-        //tfsManager.destroy();
-        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
-    }
-}
+//package com.taobao.common.tfs.testcase.function.ds;
+//
+//import java.io.FileOutputStream;
+//import java.io.OutputStream;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import com.taobao.common.tfs.testcase.rcTfsBaseCase;
+//import com.taobao.common.tfs.utility.FileUtility;
+//
+//public class SmallFileCacheTest extends rcTfsBaseCase
+//{
+//
+//  
+//    
+//	
+//    @Test
+//    public void localExsitRemoteExistSamllFile() throws Exception 
+//    {
+//        boolean bRet = false;
+//        OutputStream output = new FileOutputStream("tmp");
+//        byte [] data = FileUtility.getByte(localFile);
+//
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
+//        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
+//        
+//        String sRet;
+//        /* set local remote cache */
+//        tfsManager.setEnableLocalCache(true);
+//        tfsManager.setEnableRemoteCache(true);
+//        
+//        /* Write file */
+//        sRet = tfsManager.saveFile(data, null, null);
+//        Assert.assertNotNull(sRet);
+//        /* Read file */   
+//        tfsManager.removeLocalCache(sRet);
+//        tfsManager.removeRemoteCache(sRet);
+//        Assert.assertFalse(tfsManager.isHitLocalCache(sRet)); 
+//        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet)); 
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        
+//     
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//         
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet)); 
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
+//    }
+//    @Test
+//    public void localExistRemoteNonexsitentSamllFile() throws Exception 
+//    {
+//        boolean bRet = false;
+//        OutputStream output = new FileOutputStream("tmp");
+//        byte [] data = FileUtility.getByte(this.localFile);
+//
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
+//        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
+//        
+//        String sRet;
+//        /* set  cache */
+//        tfsManager.setEnableLocalCache(true);
+//        tfsManager.setEnableRemoteCache(true);
+//        
+//        /* Write file */
+//        sRet = tfsManager.saveFile(data, null, null);
+//        Assert.assertNotNull(sRet);
+//        /* Read file */     
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        //remove remote cache 
+//        tfsManager.removeRemoteCache(sRet);
+//        
+//        //read file  form local cache
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
+//        
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
+//    }
+//    @Test
+//    public void localNonexsitentRemoteNonexsitentSamllFile() throws Exception 
+//    {
+//        boolean bRet = false;
+//        OutputStream output = new FileOutputStream("tmp");
+//        byte [] data = FileUtility.getByte(this.localFile);
+//
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
+//        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);
+//        
+//        String sRet;
+//        /* set local cache */
+//        tfsManager.setEnableLocalCache(true);
+//        tfsManager.setEnableRemoteCache(true);
+//        
+//        /* Write file */
+//        sRet = tfsManager.saveFile(data, null, null);
+//        Assert.assertNotNull(sRet); 
+//        /* Read file */     
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp"));        
+//        
+//        //remove remote local  cache 
+//        tfsManager.removeRemoteCache(sRet);
+//        tfsManager.removeLocalCache(sRet);
+//        Assert.assertFalse(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertFalse(tfsManager.isHitRemoteCache(sRet));
+//        //read file  form ds
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        //tfsManager.destroy();
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
+//    }
+//    @Test
+//    public void localNonexsitentRemoteExistSamllFile() throws Exception 
+//    {
+//        boolean bRet = false;
+//        OutputStream output = new FileOutputStream("tmp");
+//        byte [] data = FileUtility.getByte(this.localFile);
+//
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+ "===> start");
+//        tfsManager.setRemoteCacheInfo(tairMasterAddr, tairSlaveAddr, tairGroupName, 1);      
+//        String sRet;
+//        /* Write file */
+//        sRet = tfsManager.saveFile(data, null, null);
+//        Assert.assertNotNull(sRet);
+//         
+//        /* set local remote cache */
+//        tfsManager.setEnableLocalCache(true);
+//        tfsManager.setEnableRemoteCache(true);
+//        
+//        /* Read file */     
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertEquals(FileUtility.getCrc(localFile), FileUtility.getCrc("tmp")); 
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        
+//        
+//        //remove  local  cache 
+//        tfsManager.removeLocalCache(sRet);
+//        //read file  form ds
+//        bRet = tfsManager.fetchFile(sRet, null, output);
+//        Assert.assertTrue(bRet);
+//        Assert.assertTrue(tfsManager.isHitLocalCache(sRet));
+//        Assert.assertTrue(tfsManager.isHitRemoteCache(sRet));
+//        //tfsManager.destroy();
+//        log.info(new Throwable().getStackTrace()[0].getMethodName()+"===========> end");
+//    }
+//}
