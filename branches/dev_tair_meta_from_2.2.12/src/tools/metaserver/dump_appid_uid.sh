65418e13dfbbc4d4cebcdea5ccf14d6bf368585a
hy05190134 <hy05190134@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2013-03-13 07:50:20 +0000

    [new_feature]  添加port参数，修改逻辑 see http://rb.corp.taobao.com/r/56642 rt
    git-svn-id: http://code.taobao.org/svn/tfs@2071 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/dev_tair_meta_from_2.2.12/src/tools/metaserver/dump_appid_uid.sh
+++ branches/dev_tair_meta_from_2.2.12/src/tools/metaserver/dump_appid_uid.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ $# -lt 4 ];then
-  echo "usage: $0 mysql_host mysql_user mysql_pwd mysql_dbname"
+if [ $# -lt 5 ];then
+  echo "usage: $0 mysql_host mysql_port mysql_user mysql_pwd mysql_dbname"
   exit 1
 else
   HOST=$1
-  DB_USER=$2
-  DB_PASS=$3
-  DB_NAME=$4
+  DB_PORT=$2
+  DB_USER=$3
+  DB_PASS=$4
+  DB_NAME=$5
 fi
 
 mysql_op()
@@ -15,7 +16,7 @@ mysql_op()
   sql_express=$1
   #printf "$sql_express\n"
   #/usr/bin/mysql -h$HOST -u$DB_USER -p$DB_PASS -D$DB_NAME << EOF
-  /usr/bin/mysql -u$DB_USER -p$DB_PASS -D$DB_NAME << EOF
+  /usr/bin/mysql -h$HOST -P$DB_PORT -u$DB_USER -p$DB_PASS -D$DB_NAME << EOF
   $sql_express;
   QUIT
 EOF
@@ -45,6 +46,7 @@ do
   for app_id in $data
   do
     #get uid by spec appid
+    start_uid=0
     while true
     do
       res=`mysql_op "select distinct uid from t_meta_info where app_id = $app_id and uid > $start_uid order by uid asc limit $ROW_LIMIT;" | awk 'NR > 1{print $0}'`
