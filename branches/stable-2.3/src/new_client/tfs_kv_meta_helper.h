90d95a4a91923b74bc94358a71cd1b1df0261e4d
mingyan.zc@taobao.com <mingyan.zc@taobao.com@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2013-03-25 09:44:33 +0000

    git-svn-id: http://code.taobao.org/svn/tfs@2108 b722c2b5-6a69-4643-bcd1-1a75825fa998

---
+++ branches/stable-2.3/src/new_client/tfs_kv_meta_helper.h
@@ -0,0 +1,66 @@
+/*
+ * (C) 2007-2013 Alibaba Group Holding Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *
+ * Version: $Id: tfs_meta_helper.cpp 918 2011-10-14 03:16:21Z daoan@taobao.com $
+ *
+ * Authors:
+ *      mingyan(mingyan.zc@taobao.com)
+ *      - initial release
+ *
+ */
+#ifndef TFS_CLIENT_KVMETA_HELPER_H_
+#define TFS_CLIENT_KVMETA_HELPER_H_
+
+#include <vector>
+#include <set>
+#include "common/kv_meta_define.h"
+#include "common/kv_rts_define.h"
+
+namespace tfs
+{
+  namespace client
+  {
+    class KvMetaHelper
+    {
+    public:
+      static int do_put_bucket(const uint64_t server_id, const char *bucket_name, const common::BucketMetaInfo& bucket_meta_info,
+                               const common::UserInfo &user_info);
+      static int get_table(const uint64_t server_id, common::KvMetaTable& table);
+      // TODO: parameter
+      static int do_get_bucket(const uint64_t server_id, const char *bucket_name,
+                               const char *prefix, const char *start_key, char delimiter, const int32_t limit,
+                               std::vector<common::ObjectMetaInfo> *v_object_meta_info,
+                               std::vector<std::string> *v_object_name, std::set<std::string> *s_common_prefix,
+                               int8_t *is_trucated, const common::UserInfo &user_info);
+      static int do_del_bucket(const uint64_t server_id, const char *bucket_name, const common::UserInfo &user_info);
+      static int do_head_bucket(const uint64_t server_id, const char *bucket_name, common::BucketMetaInfo *bucket_meta_info,
+                                const common::UserInfo &user_info);
+
+      static int do_put_object(const uint64_t server_id,
+                               const char *bucket_name, const char *object_name,
+                               const common::ObjectInfo &object_info, const common::UserInfo &user_info);
+      static int do_get_object(const uint64_t server_id,
+                               const char *bucket_name, const char *object_name,
+                               const int64_t offset, const int64_t length,
+                               common::ObjectInfo *object_info, bool *still_have,
+                               const common::UserInfo &user_info);
+      static int do_del_object(const uint64_t server_id,
+                               const char *bucket_name,
+                               const char *object_name,
+                               common::ObjectInfo *object_info, bool *still_have,
+                               const common::UserInfo &user_info);
+      static int do_head_object(const uint64_t server_id,
+                               const char *bucket_name,
+                               const char *object_name,
+                               common::ObjectInfo *object_info,
+                               const common::UserInfo &user_info);
+    };
+  }
+}
+
+#endif
