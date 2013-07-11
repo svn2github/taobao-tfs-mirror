1f9357d5873cfd2128cdb06f125f6447bdc9e941
linqing <linqing@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2013-07-11 08:03:45 +0000

    fix removeblk problem.
    git-svn-id: http://code.taobao.org/svn/tfs@2408 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- branches/stable-2.5/src/tools/nameserver/admintool.cpp
+++ branches/stable-2.5/src/tools/nameserver/admintool.cpp
@@ -450,21 +450,7 @@ int cmd_remove_block(const VSTRING& param)
   if(param.size() > 1)
   {
     flag = atoi(param[1].c_str());
-    if(4 == flag)
-    {
-      if(3 != param.size())
-      {
-        fprintf(stderr, "invalid parameter, if flag is 4, must have dsip:port behind\n");
-        return TFS_ERROR; 
-      }
-      server_id = Func::get_host_ip(param[2].c_str()); 
-      if (0 == server_id) 
-      { 
-        fprintf(stderr, "invalid addr %s\n", param[2].c_str()); 
-        return TFS_ERROR;
-      } 
-    }
-    else if(1 != flag && 2 != flag)
+    if(1 != flag && 2 != flag && 4 != flag)
     {
        fprintf(stderr, "removeblock's flag parameter invalid\n");
        return TFS_ERROR;
@@ -481,10 +467,7 @@ int cmd_remove_block(const VSTRING& param)
 
   send_msg_to_server(g_tfs_client->get_server_id(), &req_cc_msg, status);
 
-  if (1 == flag || 2 == flag)
-    ToolUtil::print_info(status, "removeblock: %s", param[0].c_str());
-  else//  flag=4
-    ToolUtil::print_info(status, "removeblock: %s from ds:%s", param[0].c_str(), param[2].c_str());
+  ToolUtil::print_info(status, "removeblock %s", param[0].c_str());
   return status;
 }
 
@@ -1293,7 +1276,7 @@ int cmd_dump_plan(const VSTRING& param)
 //      printf("Plan Number(running + pending):%d\n", plan_num);
 //      printf("seqno   type       status    block_id   last_update_time    runer  \n");
       printf("---------------------------------------------------------\n");
- 
+
       type_print_map.insert(make_pair(PLAN_TYPE_REPLICATE, "replicate"));
       type_print_map.insert(make_pair(PLAN_TYPE_MOVE, "move"));
       type_print_map.insert(make_pair(PLAN_TYPE_COMPACT, "compact"));
