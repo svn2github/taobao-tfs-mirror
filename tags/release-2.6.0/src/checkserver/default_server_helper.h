7a8cd6f4210d35b5f46e9b60166d5f1945ce5901
duanfei <duanfei@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2013-08-02 07:33:19 +0000

    create release-2.6.0
    git-svn-id: http://code.taobao.org/svn/tfs@2461 b722c2b5-6a69-4643-bcd1-1a75825fa998

---
+++ tags/release-2.6.0/src/checkserver/default_server_helper.h
@@ -0,0 +1,70 @@
+/*
+ * (C) 2007-2010 Alibaba Group Holding Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *
+ * Authors:
+ *   linqing <linqing.zyd@taobao.com>
+ *      - initial release
+ *
+ */
+#ifndef TFS_CHECKSERVER_DEFAULTSERVERHELPER_H
+#define TFS_CHECKSERVER_DEFAULTSERVERHELPER_H
+
+#include "tbnet.h"
+#include <Handle.h>
+#include "common/internal.h"
+#include "common/new_client.h"
+#include "common/client_manager.h"
+#include "message/message_factory.h"
+#include "common/config_item.h"
+
+#include "base_server_helper.h"
+
+namespace tfs
+{
+  namespace checkserver
+  {
+    class ServerStat
+    {
+      public:
+        ServerStat();
+        virtual ~ServerStat();
+
+        int deserialize(tbnet::DataBuffer& input, const int32_t length, int32_t& offset);
+        uint64_t id_;
+        int64_t use_capacity_;
+        int64_t total_capacity_;
+        common::Throughput total_tp_;
+        common::Throughput last_tp_;
+        int32_t current_load_;
+        int32_t block_count_;
+        time_t last_update_time_;
+        time_t startup_time_;
+        time_t current_time_;
+        common::DataServerLiveStatus status_;
+   };
+
+   class DefaultServerHelper: public BaseServerHelper
+   {
+     public:
+       DefaultServerHelper();
+       ~DefaultServerHelper();
+       int get_all_ds(const uint64_t ns_id, common::VUINT64& servers);
+       int get_block_replicas(const uint64_t ns_id, const uint64_t block_id, common::VUINT64& servers);
+       int fetch_check_blocks(const uint64_t ds_id, const common::TimeRange& range,
+           const int32_t group_count, const int32_t group_seq, common::VUINT64& blocks);
+       int dispatch_check_blocks(const uint64_t ds_id, const int64_t seqno, const common::VUINT64& blocks);
+       int get_group_info(const uint64_t ns_id, int32_t& group_count, int32_t& group_seq);
+     private:
+       int get_param_from_ns(const uint64_t ns_id, const int32_t param_index, std::string& param_value);
+   };
+
+  }
+}
+
+#endif
+
