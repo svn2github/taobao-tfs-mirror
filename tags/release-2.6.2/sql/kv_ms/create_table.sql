d991ef61662216a7a51c57019c30a8bc51db1df6
duanfei <duanfei@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2014-07-16 01:55:04 +0000

    delete this
    git-svn-id: http://code.taobao.org/svn/tfs@3063 b722c2b5-6a69-4643-bcd1-1a75825fa998

--- tags/release-2.6.2/sql/kv_ms/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `tfsmeta_1` (
-  `meta_key` varbinary(750) NOT NULL DEFAULT '',
-  `meta_value` blob NOT NULL,
-  `version` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`meta_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
