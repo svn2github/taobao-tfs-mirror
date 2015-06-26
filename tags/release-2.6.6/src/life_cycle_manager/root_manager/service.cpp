3dec0dcf59720297fb0c25594577ca6ed7314a5c
shiqing <shiqing@b722c2b5-6a69-4643-bcd1-1a75825fa998>
2015-06-26 04:45:25 +0000

    create release-2.6.6
    git-svn-id: http://code.taobao.org/svn/tfs@3404 b722c2b5-6a69-4643-bcd1-1a75825fa998

---
+++ tags/release-2.6.6/src/life_cycle_manager/root_manager/service.cpp
@@ -0,0 +1,26 @@
+/*
+ * (C) 2007-2013 Alibaba Group Holding Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *
+ * Version: $Id: service.cpp  $
+ *
+ * Authors:
+ *   qixiao <qixiao.zs@alibaba-inc.com>
+ *      - initial release
+ *
+ */
+#include <exception>
+#include <tbsys.h>
+#include <Memory.hpp>
+#include "exp_root_server.h"
+
+int main(int argc, char* argv[])
+{
+  tfs::exprootserver::ExpRootServer service;
+  return service.main(argc, argv);
+}
+
